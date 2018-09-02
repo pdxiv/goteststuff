882428c60305a7ba3c42e76578986137d449cdad
pdxiv <frank.stengard@gmail.com>
2018-09-03 00:14:57 +0200

    "improved" tcp server a bit, for the purposes of other projects

--- tcpserver.go
+++ tcpserver.go
@@ -5,67 +5,82 @@ import (
 	"net"
 )
 
+type publishMessage struct {
+	message   []byte
+	sessionID int
+}
+
 func main() {
 	newConnections := make(chan net.Conn, 128)
-	deadConnections := make(chan net.Conn, 128)
-	publishes := make(chan []byte, 128)
-	connections := make(map[net.Conn]bool)
+	deadConnections := make(chan int, 128)
+	publishes := make(chan publishMessage, 128)
+	connections := make(map[int]net.Conn)
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		panic(err)
 	}
 
 	// New incoming connection
-	go newIncomingConnection(listener, newConnections)
-
+	go checkForNewIncomingConnections(listener, newConnections)
+	connectionCounter := 0 // Used to generate session IDs
 	for {
 		select {
-		case conn := <-newConnections:
-			connections[conn] = true
-			go func() {
-				buf := make([]byte, 1024)
-				for {
-					nbyte, err := conn.Read(buf)
-					if err != nil {
-						deadConnections <- conn
-						break
-					} else {
-						fragment := make([]byte, nbyte)
-						copy(fragment, buf[:nbyte])
-						publishes <- fragment
-					}
-				}
-			}()
-			log.Print("Active connections: ", len(connections))
-		case deadConn := <-deadConnections:
-			_ = deadConn.Close()
-			delete(connections, deadConn)
-			log.Print("Active connections: ", len(connections))
+		case connection := <-newConnections:
+			connections[connectionCounter] = connection
+			go newConnectionSession(connection, publishes, deadConnections, connectionCounter)
+			connectionCounter++
+			log.Print("Number of connections: ", len(connections))
+		case deadConnection := <-deadConnections:
+			_ = connections[deadConnection].Close()
+			delete(connections, deadConnection)
+			log.Print("Number of connections: ", len(connections))
 		case publish := <-publishes:
-			for conn := range connections {
-				go func(conn net.Conn) {
-					totalWritten := 0
-					for totalWritten < len(publish) {
-						writtenThisCall, err := conn.Write(publish[totalWritten:])
-						if err != nil {
-							deadConnections <- conn
-							break
-						}
-						totalWritten += writtenThisCall
-					}
-				}(conn)
+			for session, connection := range connections {
+				if publish.sessionID != session {
+					go newPublish(publish, connection, deadConnections)
+				}
 			}
 		}
 	}
 	listener.Close()
 }
 
-func newIncomingConnection(listener net.Listener, newConnections chan net.Conn) {
+func newPublish(publish publishMessage, connection net.Conn, deadConnections chan int) {
+	totalWritten := 0
+	for totalWritten < len(publish.message) {
+		writtenThisCall, err := connection.Write(publish.message[totalWritten:])
+		if err != nil {
+			deadConnections <- publish.sessionID
+			break
+		}
+		totalWritten += writtenThisCall
+	}
+}
+
+func newConnectionSession(connection net.Conn, publishes chan publishMessage, deadConnections chan int, id int) {
+	buf := make([]byte, 1024)
+	// Wait for incoming events
 	for {
-		conn, err := listener.Accept()
+		numberOfBytes, err := connection.Read(buf)
+		if err != nil {
+			deadConnections <- id
+			break
+		} else {
+			var messageData publishMessage
+			messageData.message = make([]byte, numberOfBytes)
+			copy(messageData.message, buf[:numberOfBytes])
+			messageData.sessionID = id
+			publishes <- messageData
+		}
+	}
+}
+
+func checkForNewIncomingConnections(listener net.Listener, newConnections chan net.Conn) {
+	for {
+		connection, err := listener.Accept()
 		if err != nil {
 			panic(err)
 		}
-		newConnections <- conn
+		newConnections <- connection
 	}
 }
